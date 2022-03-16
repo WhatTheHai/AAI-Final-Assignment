3005bb6f3686d17642c653496be98b36af373da1
WhatTheHai <haiboy@hotmail.nl>
2022-03-16 11:52:37 +0100

    changed project

---
+++ AAI-Final-Assignment-WPF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AAI_Final_Assignment_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ AAI-Final-Assignment-WinForms/Form1.cs
@@ -0,0 +1,10 @@
+namespace AAI_Final_Assignment_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
