b642ec295a49a9b2cb5673a4ff540f641fb11630
WhatTheHai <haiboy@hotmail.nl>
2022-03-16 13:00:56 +0100

    added vector2d

---
+++ AAI-Final-Assignment-WinForms/util/Vector2D.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteeringCS
+{
+
+    public class Vector2D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Vector2D() : this(0, 0)
+        {
+        }
+
+        public Vector2D(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(LengthSquared());
+        }
+
+        public double LengthSquared()
+        {
+            return Math.Pow(X, 2) + Math.Pow(Y, 2);
+        }
+
+
+        public Vector2D Add(Vector2D v)
+        {
+            this.X += v.X;
+            this.Y += v.Y;
+            return this;
+        }
+
+        public Vector2D Sub(Vector2D v)
+        {
+            this.X -= v.X;
+            this.Y -= v.Y;
+            return this;
+        }
+
+        public Vector2D Multiply(double value)
+        {
+            this.X *= value;
+            this.Y *= value;
+            return this;
+        }
+
+        public Vector2D Divide(double value) {
+            if (value <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(value), $"Value cannot be 0 or lower, value is {value}");
+            }
+
+            this.X /= value;
+            this.Y /= value;
+            return this;
+        }
+
+        public Vector2D Normalize()
+        {
+            double currentLength = Length();
+            this.X /= currentLength;
+            this.Y /= currentLength;
+            return this;
+        }
+
+        public Vector2D truncate(double maX)
+        {
+            if (Length() > maX)
+            {
+                Normalize();
+                Multiply(maX);
+            }
+            return this;
+        }
+
+        public Vector2D Clone()
+        {
+            return new Vector2D(this.X, this.Y);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("({0},{1})", X, Y);
+        }
+    }
+
+
+}
