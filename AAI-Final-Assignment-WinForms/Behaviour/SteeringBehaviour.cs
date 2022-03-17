4d56f757ba760568ad0b74fc49414d40efd1c364
WhatTheHai <haiboy@hotmail.nl>
2022-03-17 12:11:23 +0100

    changed steeringbehaviour

--- AAI-Final-Assignment-WinForms/Behaviour/SteeringBehaviour.cs
+++ AAI-Final-Assignment-WinForms/Behaviour/SteeringBehaviour.cs
@@ -4,18 +4,59 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
 using AAI_Final_Assignment_WinForms.Entities;
 using AAI_Final_Assignment_WinForms.util;
 
 namespace AAI_Final_Assignment_WinForms.Behaviour
 {
-    public abstract class SteeringBehaviour
+    public class SteeringBehaviour 
     {
+        private const bool F = false;
+        public bool seek = F, flee = F, obstacleAvoidance = F;
         private MovingEntity ME { get; set; }
-        public abstract Vector2D Calculate();
+
+        public Vector2D Calculate() 
+        {
+            if (seek) {
+                return new Vector2D(0, 0);
+            }
+
+            if (flee)
+            {
+                return new Vector2D(0, 0);
+            }
+
+            if (obstacleAvoidance)
+            {
+                return new Vector2D(0, 0);
+            }
+
+            return new Vector2D(0, 0);
+        }
 
         protected SteeringBehaviour(MovingEntity me) {
             ME = me;
+        }
+
+        public Vector2D CalculateSeek() 
+        {
+            Vector2D mePos = ME.Pos.Clone();
+            Vector2D targetPos = ME.World.Witch.Pos.Clone();
+
+            Vector2D desiredVelocity = targetPos.Sub(mePos).Normalize().Multiply(ME.MaxSpeed);
+
+            return desiredVelocity.Sub(ME.Velocity.Clone());
+        }
+
+        public Vector2D CalculateHide() 
+        {
+            Vector2D mePos = ME.Pos.Clone();
+            Vector2D targetPos = ME.World.Witch.Pos.Clone();
+
+            Vector2D desiredVelocity = targetPos.Sub(mePos).Normalize().Multiply(ME.MaxSpeed);
+
+            return desiredVelocity.Sub(ME.Velocity.Clone());
         }
     }
 }
--- AAI-Final-Assignment-WinForms/World/GameWorld.cs
+++ AAI-Final-Assignment-WinForms/World/GameWorld.cs
@@ -4,8 +4,8 @@ namespace AAI_Final_Assignment_WinForms.World
 {
     public class GameWorld
     {
-
         private List<MovingEntity> entities = new List<MovingEntity>();
+        public Witch Witch { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
 
