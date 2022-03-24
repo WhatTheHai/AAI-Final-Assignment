f8614eb34915f07336d9b1dd88354b3550bbcad3
WhatTheHai <haiboy@hotmail.nl>
2022-03-24 14:52:24 +0100

    added static entity

---
+++ AAI-Final-Assignment-WinForms/Entities/StaticEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AAI_Final_Assignment_WinForms.util;
+using AAI_Final_Assignment_WinForms.World;
+
+namespace AAI_Final_Assignment_WinForms.Entities
+{
+    public class StaticEntity : BaseGameEntity
+    {
+        public int TextureWidth { get; set; }
+        public int TextureHeight { get; set; }
+
+        public StaticEntity(Vector2D pos, GameWorld world, int w, int h) : base(pos, world) {
+            TextureWidth = w;
+            TextureHeight = h;
+        }
+    }
+}
