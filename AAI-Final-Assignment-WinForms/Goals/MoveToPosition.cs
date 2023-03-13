5bda177e6e448a30de094f3a3dd40439866eac77
DennisPonstein <54404151+DennisPonstein@users.noreply.github.com>
2023-03-13 11:29:25 +0100

    new goal

--- AAI-Final-Assignment-WinForms/Goals/MoveToPosition.cs
+++ AAI-Final-Assignment-WinForms/Goals/MoveToPosition.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AAI_Final_Assignment_WinForms.Entities;
+using AAI_Final_Assignment_WinForms.Goals.Abstracts;
 
 namespace AAI_Final_Assignment_WinForms.Goals
 {
-    internal class MoveToPosition
+    public class MoveToPosition : AtomicGoal
     {
+        public MoveToPosition(MovingEntity entity) : base(entity)
+        {
+            Name = "Moving";
+        }
+
+        public override void Activate()
+        {
+            // target setten? 
+            throw new NotImplementedException();
+        }
+
+        public override void Process()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Deactivate()
+        {
+            // uitzetten 
+            throw new NotImplementedException();
+        }
     }
 }
