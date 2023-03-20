c46d63727348d79a2c6cb87245b21dc37fc950eb
WhatTheHai <haiboy@hotmail.nl>
2023-03-20 17:54:12 +0100

    added blueprint

---
+++ AAI-Final-Assignment-WinForms/Fuzzy/FuzzyModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAI_Final_Assignment_WinForms.Fuzzy
+{
+    internal class FuzzyModule
+    {
+    }
+}
---
+++ AAI-Final-Assignment-WinForms/Fuzzy/FuzzyTerm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAI_Final_Assignment_WinForms.Fuzzy
+{
+    abstract class FuzzyTerm {
+        public abstract FuzzyTerm Clone();
+
+        public abstract float GetDOM();
+
+        public abstract void ClearDom();
+
+        public abstract void ORwithDOM(float value);
+    }
+}
