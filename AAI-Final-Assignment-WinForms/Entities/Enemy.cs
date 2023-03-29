cd0a7f1895d340ef72f398991636b351603c4d77
WhatTheHai <haiboy@hotmail.nl>
2023-03-29 11:41:10 +0200

    Cleanup

--- AAI-Final-Assignment-WinForms/Entities/Enemy.cs
+++ AAI-Final-Assignment-WinForms/Entities/Enemy.cs
@@ -47,9 +47,6 @@ namespace AAI_Final_Assignment_WinForms.Entities
 
         public override void Render(Graphics g)
         {
-            // g.DrawImage(Texture, (int)Pos.X - TextureWidth / 2, (int)Pos.Y - TextureHeight / 2);
-
-            // g.FillEllipse(Brushes.Blue, new Rectangle((int)Pos.X, (int)Pos.Y, 3, 3));
             g.DrawEllipse(new Pen(Color.Red, 3),
                 new Rectangle((int)Pos.X - (int)Radius, (int)Pos.Y - (int)Radius, (int)Radius * 2, (int)Radius * 2));
 
